ea9db5735f9240f52b3352dfa72f55fcb34ba7fa
smanch3v <38432551+smanch3v@users.noreply.github.com>
2021-01-16 13:47:21 +0200

    a

--- index.php
+++ index.php
@@ -18,13 +18,13 @@
     ?>
     <div class="container">
         <div class="row">
-            <div class="col-sm">
+            <div class="col-sm-3">
                 One of three columns
             </div>
-            <div class="col-sm">
+            <div class="col-sm-3">
                 One of three columns
             </div>
-            <div class="col-sm">
+            <div class="col-sm-3">
                 One of three columns
             </div>
         </div>
